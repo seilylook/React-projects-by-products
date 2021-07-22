52a7015614615198ebf211fc32b008710a390198
seilylook@naver.com <seilylook@naver.com>
2021-07-22 11:54:03 +0900

    component addtutorials complete

--- src/reducers/tutorials.js
+++ src/reducers/tutorials.js
@@ -17,5 +17,28 @@ function tutorialReducer(tutorials = initialState, action) {
 
     case RETRIEVE_TUTORIALS:
       return payload;
+
+    case UPDATE_TUTORIAL:
+      return tutorials.map(tutorial => {
+        if (tutorial.id === payload.id) {
+          return {
+            ...tutorial,
+            ...payload
+          };
+        } else {
+          return tutorial;
+        }
+      });
+
+    case DELETE_TUTORIAL:
+      return tutorials.filter(({ id }) => id !== payload.id);
+
+    case DELETE_ALL_TUTORIALS:
+      return [];
+
+    default:
+      return tutorials;
   }
 }
+
+export default tutorialReducer;
