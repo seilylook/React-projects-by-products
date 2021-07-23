4d4fe6ee3bdcbdcdf40a8d5e1e483d6cf936ceeb
seilylook@naver.com <seilylook@naver.com>
2021-07-23 10:17:19 +0900

    tutorial-finished

--- src/App.js
+++ src/App.js
@@ -3,8 +3,40 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
+import AddTutorial from "./components/AddTutorial";
+import Tutorial from "./components/Tutorial";
+import TutorialList from "./components/TutorialList";
+
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          bezKoder
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+          <Route exact path="/add" component={AddTutorial} />
+          <Route path="/tutorials/:id" component={Tutorial} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
